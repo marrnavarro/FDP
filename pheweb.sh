f35f7dfd52e43be0dbf4c13d54186f129e707bb6
marrnavarro <65028896+marrnavarro@users.noreply.github.com>
2021-06-16 09:07:18 +0200

    Add files via upload

---
+++ pheweb.sh
@@ -0,0 +1,14 @@
+activate-pheweb-py3.6
+cd /imppc/labs/dnalab/share/mar_navarro/pheweb_data/
+
+### Creating a working instance with sample data
+pheweb phenolist glob --simple-phenocode 'tests/input_files/assoc-files/*'
+pheweb phenolist unique-phenocode
+pheweb phenolist read-info-from-association-files
+pheweb phenolist import-phenolist -f pheno-list-categories.json tests/input_files/categories.csv
+pheweb phenolist merge-in-info pheno-list-categories.json
+pheweb phenolist verify --required-columns category
+
+pheweb process
+
+pheweb serve --port 5002
